3227ca4edc05f261f61cd6ad41694748763a240c
levykkn <levykkostya@gmail.com>
2025-07-27 13:53:51 +0100

    Introduce DAL, hot-reloading, and declarative router

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,4 +3,6 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 3000,
   PUBLIC_DIR: require('path').join(__dirname, '..', '..', 'frontend'),
+  // Add NODE_ENV to config, defaulting to 'production' for safety.
+  NODE_ENV: process.env.NODE_ENV || 'production',
 };
