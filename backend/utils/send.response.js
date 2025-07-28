257ab33def50ed5a1ff39385bf3046099ab96d92
levykkn <levykkostya@gmail.com>
2025-07-28 12:20:29 +0100

    implement full course CRUD service
    Adds the business logic to the CourseService for creating, updating,
    and deleting courses.
    
    - Implements `createCourse` to add new courses with a unique ID.
    - Implements `updateCourse` to find and modify existing courses.
    - Implements `deleteCourse` to remove courses from the data file.
    - All methods correctly interface with the Data Access Layer (DAL)
      to persist changes to the JSON file.

--- backend/utils/send.response.js
+++ backend/utils/send.response.js
@@ -2,8 +2,9 @@ module.exports = function sendResponse(res, statusCode, data, contentType = 'app
   res.writeHead(statusCode, {
       'Content-Type': contentType,
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      // CRITICAL FIX: Added PUT and DELETE to the list of allowed methods.
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
   });
   res.end(contentType === 'application/json' ? JSON.stringify(data) : data);
-}; 
+};
